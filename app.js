370197502ad6d4251f89c609077868321c40d06a
Tony Kowalewski <tony@kowalewski.dev>
2022-05-30 23:58:55 +0200

    add test rout and package-lock.json

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.send('Hello there');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}...`);
+});

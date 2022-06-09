69bd27e1249968d558649465cf7c686caf330c0b
Tony Kowalewski <tony@kowalewski.dev>
2022-06-09 19:01:42 +0200

    update getTask & getTasks controllesrs

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,11 +1,22 @@
 const Task = require('../models/task');
 
-const getTasks = (req, res) => {
-  res.status(200).send('List of all tasks');
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (err) {
+    res.status(500).json({ msg: err });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    // const task = await Task.findById(req.params.id);
+    res.status(200).json({ task });
+  } catch (err) {
+    res.status(404).json({ msg: `Task with id ${req.params.id} not found` });
+  }
 };
 
 const addTask = async (req, res) => {

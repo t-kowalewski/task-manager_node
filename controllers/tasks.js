b5372bdd75f42631aa8603b0ff82b5d0b6be44b6
Tony Kowalewski <tony@kowalewski.dev>
2022-06-14 13:02:04 +0200

    add asyncWrapper middleware

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,76 +1,62 @@
 const Task = require('../models/task');
+const { asyncWrapper } = require('../middleware/async');
 
-const getTasks = async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.status(200).json({ tasks });
-  } catch (err) {
-    res.status(500).json({ msg: err });
+const getTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+});
+// const getTasks = async (req, res) => {
+//   try {
+//     const tasks = await Task.find({});
+//     res.status(200).json({ tasks });
+//   } catch (err) {
+//     res.status(500).json({ msg: err });
+//   }
+// };
+
+const getTask = asyncWrapper(async (req, res) => {
+  // const task = await Task.findOne({ _id: req.params.id });
+  const task = await Task.findById(req.params.id);
+  // find..() returns null if search query has same length as correct id but can't be found - we handle it
+  if (!task) {
+    return res
+      .status(404)
+      .json({ msg: `Task with id ${req.params.id} not found` });
   }
-};
 
-const getTask = async (req, res) => {
-  try {
-    // const task = await Task.findOne({ _id: req.params.id });
-    const task = await Task.findById(req.params.id);
-    // find..() returns null if search query has same length as correct id but can't be found - we handle it
-    if (!task) {
-      return res
-        .status(404)
-        .json({ msg: `Task with id ${req.params.id} not found` });
-    }
+  res.status(200).json({ task });
+});
 
-    res.status(200).json({ task });
-  } catch (err) {
-    res.status(404).json({ msg: `Task with id ${req.params.id} not found` });
-  }
-};
+const addTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+});
 
-const addTask = async (req, res) => {
-  try {
-    const task = await Task.create(req.body);
-    res.status(201).json({ task });
-  } catch (err) {
-    res.status(400).json({ completed: false, msg: err.message });
-  }
-};
+const editTask = asyncWrapper(async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
-const editTask = async (req, res) => {
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!task) {
-      return res.status(404).json({
-        completed: false,
-        msg: `task with id ${req.params.id} not found`,
-      });
-    }
-
-    res.status(201).json({ completed: true, task });
-  } catch (err) {
-    res.status(500).json({
+  if (!task) {
+    return res.status(404).json({
       completed: false,
-      msg: err,
+      msg: `task with id ${req.params.id} not found`,
     });
   }
-};
 
-const deleteTask = async (req, res) => {
-  try {
-    const deletedObj = await Task.findOneAndDelete({ _id: req.params.id });
-    if (!deletedObj) {
-      return res
-        .status(404)
-        .json({ msg: `task with id ${req.params.id} not found` });
-    }
+  res.status(201).json({ completed: true, task });
+});
 
-    res.status(200).json({ completed: true, deletedObj });
-  } catch (err) {
-    res.status(404).json({ msg: `task with id ${req.params.id} not found` });
+const deleteTask = asyncWrapper(async (req, res) => {
+  const deletedObj = await Task.findOneAndDelete({ _id: req.params.id });
+  if (!deletedObj) {
+    return res
+      .status(404)
+      .json({ msg: `task with id ${req.params.id} not found` });
   }
-};
+
+  res.status(200).json({ completed: true, deletedObj });
+});
 
 module.exports = { getTasks, getTask, addTask, editTask, deleteTask };
---
+++ middleware/async.js
@@ -0,0 +1,11 @@
+const asyncWrapper = (fn) => {
+  return async function (req, res, next) {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+module.exports = { asyncWrapper };
